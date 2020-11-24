741f27938f719ff6d3e61a4c39df919aa3a2505d
pvarentsov <varentsovpavel@gmail.com>
2020-11-24 22:43:29 +0300

    Getting started with RxJS: Update gitignore

--- RxJS/dist/Main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UserStore_1 = require("./store/UserStore");
-new UserStore_1.UserStore()
-    .list()
-    .subscribe(value => console.log(value));
-//# sourceMappingURL=Main.js.map
