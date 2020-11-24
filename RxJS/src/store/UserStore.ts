4ec73d263ba2519e0cc97f81afa0446137b85f1d
pvarentsov <varentsovpavel@gmail.com>
2020-11-25 01:00:17 +0300

    Getting started with RxJS: Code refactoring

--- RxJS/src/store/UserStore.ts
+++ RxJS/src/store/UserStore.ts
@@ -3,31 +3,25 @@ import { reduce, take } from 'rxjs/operators';
 import { User } from '../model/User';
 import { whereAnd } from './operator/StoreOperators';
 
-type UserId = string;
-
 export class UserStore {
 
-  private readonly users: Map<UserId, User> = new Map([
-    ['1', new User('1', 'nick_1', 'pass')],
-    ['2', new User('2', 'nick_2', 'pass')],
-    ['3', new User('3', 'nick_3', 'pass')]
-  ]);
+  private readonly users: User[] = [
+    new User('1', 'nick_1', 'pass'),
+    new User('2', 'nick_2', 'pass'),
+    new User('3', 'nick_3', 'pass'),
+  ];
 
   public one(filter: {id?: string}): Observable<User> {
-    return this.from().pipe(
+    return from(this.users).pipe(
       whereAnd(filter),
       take(1),
     );
   }
 
   public list(): Observable<User[]> {
-    return this.from().pipe(
-      reduce<User, User[]>((acc, value) => [...acc, value], [])
+    return from(this.users).pipe(
+      reduce<User, User[]>((acc, user) => [...acc, user], [])
     );
-  }
-
-  private from(): Observable<User> {
-    return from(Array.from(this.users.values()));
   }
 
 }
