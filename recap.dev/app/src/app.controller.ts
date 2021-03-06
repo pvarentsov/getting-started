a6b0ceb6871a0c7c58b59217d6603fa562cdfd47
pvarentsov <varentsovpavel@gmail.com>
2021-03-06 22:02:10 +0300

    Getting started with Recap.Dev: Add http request example

--- recap.dev/app/src/app.controller.ts
+++ recap.dev/app/src/app.controller.ts
@@ -1,19 +1,26 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   @HttpCode(200)
-  getHello(): string {
-    return this.appService.getHello();
+  greet(): string {
+    return this.appService.greet();
   }
 
   @Get('error')
   @HttpCode(500)
-  getError(): never {
-    return this.appService.getError();
+  throwError(): never {
+    return this.appService.throwError();
+  }
+
+  @Get('request')
+  @HttpCode(200)
+  request(): Observable<any> {
+    return this.appService.request();
   }
 }
--- recap.dev/app/src/app.service.ts
+++ recap.dev/app/src/app.service.ts
@@ -1,18 +1,32 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpService, Injectable, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private httpService: HttpService) {}
+
+  greet(): string {
     Logger.error('[Logger.error]: Hello World!');
     console.error('[console.log]: Hello World!');
 
     return 'Hello World!';
   }
 
-  getError(): never {
+  throwError(): never {
     Logger.error('[Logger.error]: Error');
     console.error('[console.log]: Error');
 
     throw new Error('Error!');
+  }
+
+  request(): Observable<any> {
+    return this.httpService.get('http://localhost:3000/hello').pipe(
+      tap(() => {
+        Logger.log(`[Logger.error]: Request`);
+        console.log('[console.log]: Request');
+      }),
+      map(res => res.data)
+    );
   }
 }
